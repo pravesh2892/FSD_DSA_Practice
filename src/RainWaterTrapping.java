a6696060367aeac58a3dc84de7392694cc1e61a7
pravesh meena <pravesh.meena2892@gmail.com>
2023-02-10 09:01:06 +0530

    try to commite

---
+++ src/RainWaterTrapping.java
@@ -0,0 +1,26 @@
+public class RainWaterTrapping {
+    public static void main(String[] args) {
+        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        System.out.println(trap(height));
+    }
+
+    public static int trap(int[] height) {
+        int leftMax = 0, rightMax = 0;
+        int left = 0, right = height.length - 1;
+        int waterSum = 0;
+        while (left <= right) {
+            if (leftMax <= rightMax) {
+                waterSum += Math.max(leftMax - height[left], 0);
+                leftMax = Math.max(leftMax, height[left]);
+                left++;
+            } else {
+                waterSum += Math.max(rightMax - height[right], 0);
+                rightMax = Math.max(rightMax, height[right]);
+                right--;
+
+            }
+
+        }
+        return waterSum;
+    }
+}

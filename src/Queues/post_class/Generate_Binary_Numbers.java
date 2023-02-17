a3cc44bbb498899d260c652dec81e56cd0a2b489
pravesh meena <pravesh.meena2892@gmail.com>
2023-02-17 08:20:13 +0530

    opration on queue

---
+++ src/Queues/post_class/Generate_Binary_Numbers.java
@@ -0,0 +1,20 @@
+package Queues.post_class;
+import java.util.*;
+public class Generate_Binary_Numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        generateBinaryNumbers(n);
+    }
+    public static void generateBinaryNumbers(int n){
+        Queue<String> queue = new ArrayDeque<>();
+        queue.add("1");
+        int i=1;
+        while(i++ <=n){
+            queue.add(queue.peek() +'0');
+            queue.add(queue.peek() +'1');
+
+            System.out.print(queue.poll()+ ' ');
+        }
+    }
+}

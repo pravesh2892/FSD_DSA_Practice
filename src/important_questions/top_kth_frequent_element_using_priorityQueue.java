a24fdcda38fb552d4d79cd7dd5d647a24562f8b8
pravesh meena <pravesh.meena2892@gmail.com>
2023-03-10 22:10:40 +0530

    top kth frequent element using priority queue

--- src/important_questions/top_kth_frequent_element_using_priorityQueue.java
+++ src/important_questions/top_kth_frequent_element_using_priorityQueue.java
@@ -1,10 +1,10 @@
 package important_questions;
 import  java.util.*;
-public class top_kth_frequent_element_using_priorityQueue {
+public class top_kth_frequent_element_using_priorityQueue {   //8 5 3 4 9
     public static void main(String[] args) {
         int[]arr = {1,3,2,4,2,1,7,8,8,8,4,7,4,8,4};
         int k=3;
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>(); // 1-1,3-1, 2-2,4-1,
         for(Integer element :arr){
             map.put(element, map.getOrDefault(element, 0)+1);
         }
